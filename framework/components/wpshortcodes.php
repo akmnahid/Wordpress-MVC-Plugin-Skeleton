d86b4bea2571e621b1936524b00459be306f915d
akmnahid <mail@akmnahid.com>
2015-04-07 15:23:13 +0600

    Ini Commit Ofc

---
+++ framework/components/wpshortcodes.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * User: Nahid Hossain
+ * Website: http://www.akmnahid.com , http://www.somogrotech.com
+ * Contact: http://www.somogrotech.com/contact-us/
+ * Date: 7/4/2015
+ * Time: 1:52 PM
+ */
