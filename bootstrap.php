f560741af6c3356e7d8b9fc9d917848f73112012
akmnahid <mail@akmnahid.com>
2015-04-07 09:28:07 +0600

    First Commit

---
+++ bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Name of the plugin, must be unique.
+ * Plugin URI: https://github.com/akmnahid/Wordpress-MVC-Plugin-Skeleton
+ * Description: A Simple MVC Skeleton for Wordpress Plugin Development
+ * Version: 1.0.1
+ * Author: Nahid Hossain
+ * Author URI: http://www.akmnahid.com
+ * License: GPL2
+ */
+
+
+defined('ABSPATH') or die('No script kiddies please!');
